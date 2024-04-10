77cf3baeb0c90d873a08a9a3ee5ea5c14e29fadc
c23io1060 <154226923+c23io1060@users.noreply.github.com>
2024-04-10 15:21:22 +0800

    test

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-=======
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
->>>>>>> origin/main
-  }; 
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
